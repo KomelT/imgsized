760ad98b00d75905cc33e7f7de3241fa7b9691d5
Tilen Komel <tilen.komel10@gmail.com>
2022-04-02 00:12:49 +0200

    .prettierrc.json added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-const { checkEnv, randomStr } = require('./helper');
+const { checkEnv, randomStr } = require('./helper')
 
-console.log("Hello from ImgSized API!")
-console.log(".env check:")
+console.log('Hello from ImgSized API!')
+console.log('.env check:')
 
 // Check and set environment variables
-checkEnv("IMGSIZED_MAX_UPLOAD_SIZE", 20, "MB")
-checkEnv("IMGSIZED_FILE_TTL", 3600, "s")
-checkEnv("IMGSIZED_API_KEYS", randomStr(30), "")
+checkEnv('IMGSIZED_MAX_UPLOAD_SIZE', 20, 'MB')
+checkEnv('IMGSIZED_FILE_TTL', 3600, 's')
+checkEnv('IMGSIZED_API_KEYS', randomStr(30), '')
--- helper.js
+++ helper.js
@@ -1,27 +1,27 @@
-require("dotenv").config();
+require('dotenv').config()
 
 function checkEnv(envName, defaultValue, unit) {
-  if (process.env[envName]) console.log(`- ${envName} Exists!`);
-  else {
-    process.env[envName] = defaultValue;
-    console.log(
-      `- IMGSIZED_MAX_UPLOAD_SIZE Is missing! Now is: ${process.env[envName]} ${unit}`
-    );
-  }
+   if (process.env[envName]) console.log(`- ${envName} Exists!`)
+   else {
+      process.env[envName] = defaultValue
+      console.log(
+         `- IMGSIZED_MAX_UPLOAD_SIZE Is missing! Now is: ${process.env[envName]} ${unit}`
+      )
+   }
 }
 
 function randomStr(length) {
-  var result = "";
-  var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
+   var result = ''
+   var characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+   var charactersLength = characters.length
+   for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
+   }
+   return result
 }
 
 module.exports = {
-  checkEnv: checkEnv,
-  randomStr: randomStr,
-};
+   checkEnv: checkEnv,
+   randomStr: randomStr,
+}
