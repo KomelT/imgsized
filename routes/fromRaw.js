1c3330d1f06948d8aae46e8a00dd6fc0ddbfb88e
Tilen Komel <tilen.komel10@gmail.com>
2022-04-10 16:55:42 +0200

    /fromRaw working!

--- routes/fromRaw.js
+++ routes/fromRaw.js
@@ -1,11 +1,62 @@
 const express = require('express')
 const router = express.Router()
+const { exec } = require('child_process')
+const fs = require('fs')
 
 router.post('/', (req, res) => {
-   console.log('Filename:' + req.files.foo.name)
-   console.log('Mimetype:' + req.files.foo.mimetype)
-   console.log('File size:' + req.files.foo.size)
-   res.send('Ok')
+   let files = []
+   req.files.foo.forEach((el) => {
+      files.push({
+         origName: el.name,
+         origSize: el.size,
+         errMessage: [],
+         done: false,
+      })
+   })
+
+   req.files.foo.forEach((el, i) => {
+      fs.writeFile('./data/' + el.name, Buffer.from(el.data, 'hex'), (err) => {
+         if (err) {
+            files[i].errMessage.push('Error while uploading files to server!')
+            files[i].done = true
+         }
+      })
+   })
+
+   console.log('Done uploading!')
+
+   files.forEach((el, i) => {
+      if (el.errMessage.length == 0) {
+         const command = 'rawtherapee-cli -Y -c ./data/' + el.origName
+         exec(command, (error, stdout, stderr) => {
+            if (error || stderr) {
+               console.log(`stderr: ${stderr}`)
+               console.log(`stderr: ${error}`)
+               files[i].errMessage.push(
+                  'Error while converting from RAW to JPG!'
+               )
+               files[i].done = true
+            } else {
+               console.log(`stdout: ${stdout}`)
+               files[i].done = true
+            }
+         })
+      }
+   })
+   let count = 0
+   const inte = setInterval(() => {
+      if (files.length == count) {
+         clearInterval(inte)
+         files.forEach((el, i) => {
+            delete files[i].done
+         })
+         res.send(files)
+      }
+      count = 0
+      files.forEach((el) => {
+         if (el.done) count++
+      })
+   })
 })
 
 module.exports = router
