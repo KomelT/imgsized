ccf8e14aba3100bed1c870e30077f52ef9d4d9ea
Tilen Komel <tilen.komel10@gmail.com>
2022-04-10 17:38:50 +0200

    /download path added

--- app.js
+++ app.js
@@ -8,20 +8,25 @@ const { checkEnv, randomStr } = require('./helper')
 
 // Welcome message
 console.log('Hello from ImgSized API!')
-console.log('.env check:')
 
 // Check and set environment variables
+console.log('.env check:')
 checkEnv('IMGSIZED_MAX_UPLOAD_SIZE', 20, 'MB')
 checkEnv('IMGSIZED_FILE_TTL', 3600, 's')
 checkEnv('IMGSIZED_API_KEYS', randomStr(30), '')
+checkEnv('IMGSIZED_APP_PUBLIC_DOMAIN', 'http://localhost/', '')
 checkEnv('IMGSIZED_APP_PORT', 3000, '')
 
-// Middleware
+// Middlewares
 app.use(fileUpload())
 
 // Import routes
 const fromRaw = require('./routes/fromRaw')
+const download = require('./routes/download')
+
 app.use('/fromRaw', fromRaw)
+app.use('/download', download)
+
 app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, './frontend/index.html'))
 })
